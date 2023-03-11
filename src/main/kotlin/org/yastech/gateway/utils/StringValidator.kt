1dd662ca87461405bffe1f833969229a24d700cc
Hossein Araghi <hoseinaraghi84@gmail.com>
2023-03-12 01:07:55 +0330

    Email validator

--- src/main/kotlin/org/yastech/gateway/utils/StringValidator.kt
+++ src/main/kotlin/org/yastech/gateway/utils/StringValidator.kt
@@ -1,18 +1,13 @@
 package org.yastech.gateway.utils
 
-import org.springframework.stereotype.Service
 import java.util.regex.Pattern
 
 
-@Service
-class StringValidator
-{
-    fun isValidEmail(email: String): Boolean {
-        val pattern = ("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+fun isValidEmail(email: String): Boolean {
+    val pattern = ("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
 
-        return Pattern.compile(pattern)
-            .matcher(email)
-            .matches()
-    }
+    return Pattern.compile(pattern)
+        .matcher(email)
+        .matches()
 }
