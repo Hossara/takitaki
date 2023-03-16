1419e463c1b860b3ed71ff44a43c6130f5e220b5
Hossein Araghi <hoseinaraghi84@gmail.com>
2023-03-17 03:24:39 +0330

    validation and responses

--- webapp/pages/index.vue
+++ webapp/pages/index.vue
@@ -4,13 +4,54 @@ definePageMeta({ layout: "auth" })
 useHead({ title: "TakiTaki - Login" })
 
 const registerModal = new ModalHandler()
+
+const toast = useNuxtApp().$toast  
+const axios = useNuxtApp().$axios 
+
+onMounted(() =>
+{
+    const query = useRoute().query
+    
+    switch(query["res"])
+    {
+        case "done":
+            toast.success("Your account has been created successfully! Please activate your account before expiration and login!")
+            break
+        case "validate":
+            const id = query["id"]
+            const email = query["email"]
+
+            if(id && email)
+            {
+                axios
+                    .get(`/auth/validate/${email}/${id}`)
+                    .then((value) => 
+                    {
+                        const data = value.data
+
+                        if(data["status"] === "success")
+                        {
+                            location.href = "/?res=reg_done"
+                        }
+                        else if(data["code"] === "inv_usr")
+                        {
+                            toast.error("Registration link is not valid!")
+                        }
+                    })
+            }
+            else toast.error("Registration link is not valid!")
+            break
+        case "reg_done":
+            toast.success("You registered successfully! ")
+    }
+})
 </script>
 
 <template>
     <div id="index" class="flex w-full justify-center">
         <ModalRegister :show="registerModal.status.value" @closeModal="registerModal.hide"/>
 
-        <form action="post" class="pl-3 pt-3 pr-3 pb-4 shadow-md rounded-md bg-white max-w-[400px] w-full text-center">
+        <form method="post" class="pl-3 pt-3 pr-3 pb-4 shadow-md rounded-md bg-white max-w-[400px] w-full text-center">
             <TkInput class="w-full mb-4" placeholder="Email or phone number"/>
 
             <TkInput class="w-full mb-4" placeholder="Password" type="password"/>
